b6554458103ff86eacac573f56dc5eb190214aee
Mattia Ceccarelli <41483077+Mat092@users.noreply.github.com>
2018-08-23 18:23:31 +0200

    Add files via upload

--- Algorithm.py
+++ Algorithm.py
@@ -2,50 +2,85 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.datasets import make_moons, make_circles#, make_classification
 from sklearn import datasets
-#from random import randint
+from random import randint
 import MLPClassifier_ as MLP
 
 def algorithm():
-    generation = MLP.RNet_population()
+    generation = MLP.RNet_population(4)
     gen = 1
     
-    while generation.Best_Score() < 1: #continua fino a fitness = 1
+    #dataset = make_circles(n_samples = 100,noise=0.2,factor = 0.5, random_state = 1)
+    #rnstate = randint(1,100)
+    dataset = make_moons(n_samples = 100, noise = 0.27,random_state=1)
         
         
-        dataset = make_moons(n_samples = 100, noise=0.3, random_state=0)
-        digits = datasets.load_digits()
-        #make_circles(noise=0.2, factor=0.5, random_state=1)
+    #per datasets
+    X, y = dataset
+    X = StandardScaler().fit_transform(X)
+    X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=.4, random_state=1)
         
-        #per make_moons
-        X, y = dataset
-        X = StandardScaler().fit_transform(X)
-        X_train, X_test, y_train, y_test = \
-            train_test_split(X, y, test_size=.4, random_state=42)
-        
-#        #per numeri scritti a mano:
+    #        #per numeri scritti a mano:
+#        digits = datasets.load_digits()   
 #        n_samples = len(digits.images)
 #        Data = digits.images.reshape((n_samples,-1))
 #        Target = digits.target
 #        Data_train, data_test, target_train, target_test = \
 #            train_test_split(Data,Target,test_size =.4,random_state = 42)
+    
+    while generation.Best_Score() < 1: #continua fino a fitness = 1
         
         if gen == 1: 
-           #generation.Print_pop() 
            generation.pop_fitness(X_train, X_test, y_train, y_test)
+           generation.medium_pop_fitness()
            generation.shortBubbleSort()
+           print ('\n',"Generazione",'\t', gen)
+           print("num_Individuals = ", generation.num_individuals,'\n')
+           #generation.Print_pop()
+           print("************Best Net**********")
+           generation.Print_Best()
+           print("********************************")
         
+        Next_gen = MLP.RNet_population(0)
         #Creazione della nuova generazione
-        Next_gen = MLP.RNet_population()
-        for i in range(generation.num_individuals):
-            if len(Next_gen.RNpopulation) >= generation.num_individuals:
-                break
-            for j in range(i,generation.num_individuals):
-                if len(Next_gen.RNpopulation) >= generation.num_individuals:
-                    break
-                tpl = generation.RNpopulation[i].Crossover1(generation.RNpopulation[j])
-                Next_gen.RNpopulation.append(tpl[0])
-                Next_gen.RNpopulation.append(tpl[1])
         
+        for i in range(len(generation.RNpopulation)):
+            if len(Next_gen.RNpopulation) >= len(generation.RNpopulation):
+                break
+            rnd = randint(i,len(generation.RNpopulation)-1)
+            tpl = generation.RNpopulation[i].Crossover1(generation.RNpopulation[rnd])
+            Next_gen.RNpopulation.append(tpl[0])
+            Next_gen.RNpopulation.append(tpl[1])
+            
+            
+#            for j in range(i + 1,len(generation.RNpopulation)):
+#                if len(Next_gen.RNpopulation) >= len(generation.RNpopulation):
+#                    break
+#                tpl =generation.RNpopulation[i].Crossover1(generation.RNpopulation[j])
+#                Next_gen.RNpopulation.append(tpl[0])
+#                Next_gen.RNpopulation.append(tpl[1])
+                
+        
+#        #Creazione della nuova generazione
+#        parents = MLP.RNet_population(0)
+#        for i in range(generation.num_individuals):    #Seleziono i genitori
+#            prb = randint(0,85) / 100
+#            if generation[i].fitness > prb:
+#                parents.RNpopulation.append(generation[i])
+#        parents.num_individuals = len(parents.RNpopulation)   
+#        
+#        for i in range(parents.num_individuals):
+#            if len(Next_gen.RNpopulation) >= generation.num_individuals:
+#                break
+#            for j in range(i,parents.num_individuals):
+#                if len(Next_gen.RNpopulation) >= generation.num_individuals:
+#                    break
+#                tpl = parents[i].Crossover1(parents[j])
+#                Next_gen.RNpopulation.append(tpl[0])
+#                Next_gen.RNpopulation.append(tpl[1])
+        
+#        Next_gen.num_individuals = len(Next_gen.RNpopulation) 
+            
         generation = Next_gen
         del Next_gen
         
@@ -58,12 +93,20 @@ def algorithm():
         #Calcolo fitness e ordino gli individui 
         generation.medium_pop_fitness()
         generation.shortBubbleSort()
-        print('\n',"************Best Net**********")
+        
+        gen = gen + 1
         print ("Generazione",'\t', gen)
+        print("num_Individuals = ", len(generation.RNpopulation),'\n')
         #generation.Print_pop()
+        print("************Best Net**********")
         generation.Print_Best()
         print("********************************")
-        gen = gen + 1
+        
 
 algorithm()
+
+
+    
+
+
 
--- MLPClassifier_.py
+++ MLPClassifier_.py
@@ -7,15 +7,19 @@ from random import randint
 
 class RNet_population:
     
-    def __init__(self):
+    def __init__(self,num_individuals = 10):
         self.medium_fitness = 0
-        self.num_individuals = 10
+        self.num_individuals = num_individuals
         self.RNpopulation = []
         for n in range(self.num_individuals):
-            self.RNpopulation.append(Random_Network())
+            self.RNpopulation.append(Random_Network())    
             
-#    def __getitem__(self,index):
-#        return self.RNpopulation[index]         
+    def __getitem__(self,index):
+        return self.RNpopulation[index]         
+
+    def pop_Neurons_product(self):
+        for ind in range (len(self.RNpopulation)):
+            self.RNpopulation[ind].cal_Neurons_product()
     
     def pop_fitness(self, X_train, X_test, y_train, y_test): #calcola il fitness
         for RandNet in self.RNpopulation:
@@ -25,17 +29,25 @@ class RNet_population:
         fitness_tot = 0
         for RandNet in self.RNpopulation:
             fitness_tot = fitness_tot + RandNet.fitness
-        self.medium_fitness = fitness_tot / self.num_individuals
+        self.medium_fitness = fitness_tot / len(self.RNpopulation)
        
-    def Bubble_sort(self): #slow
+    def BubbleSort(self): #se voglio ordinarli anche per numero di neuroni
+        self.pop_Neurons_product()
         sort = Random_Network()
         for ind1 in range(len(self.RNpopulation)-1):
             for ind2 in range(ind1,len(self.RNpopulation)-1,1):
-                if self.RNpopulation[ind1].fitness > self.RNpopulation[ind2].fitness:
+                if self.RNpopulation[ind1].fitness < self.RNpopulation[ind2].fitness:
                     sort = self.RNpopulation[ind1]
-                    self.RNpopulation[ind2] = self.RNpopulation[ind1]
-                    self.RNpopulation[ind1] = sort
-    
+                    self.RNpopulation[ind1] = self.RNpopulation[ind2]
+                    self.RNpopulation[ind2] = sort
+                elif (self.RNpopulation[ind1].fitness == 
+                      self.RNpopulation[ind2].fitness and 
+                      self.RNpopulation[ind1].Neurons_product > 
+                      self.RNpopulation[ind2].Neurons_product):
+                    sort = self.RNpopulation[ind1]
+                    self.RNpopulation[ind1] = self.RNpopulation[ind2]
+                    self.RNpopulation[ind2] = sort
+                    
     def shortBubbleSort(self): #ordina la popolazione per fitness
         exchanges = True
         passnum = len(self.RNpopulation)-1
@@ -46,8 +58,8 @@ class RNet_population:
                    exchanges = True
                    temp = self.RNpopulation[i]
                    self.RNpopulation[i] = self.RNpopulation[i+1]
-                   self.RNpopulation[i+1] = temp
-           passnum = passnum-1
+                   self.RNpopulation[i+1] = temp    
+           passnum = passnum - 1
         
     def Print_Best(self): #After Bubble_sort
         print("mean fitness: ",self.medium_fitness)
@@ -67,6 +79,7 @@ class Random_Network:
     def __init__(self):
         self.fitness = 0
         self.genome = []
+        self.Neurons_product = 0
         self.num_hidden_layers = randint(1,10)
         for n in range(self.num_hidden_layers):
             self.genome.append(randint(1,100))
@@ -75,7 +88,8 @@ class Random_Network:
         #Metodi di MLPCassifier 
         tpl = tuple(self.genome)
         net = MLPClassifier(hidden_layer_sizes = tpl, activation = 'relu', alpha = 1,
-                            learning_rate = 'constant' , learning_rate_init = 0.001)
+                            learning_rate = 'constant' , learning_rate_init = 0.001,
+                            random_state = 1)
         #MLPClassifier(hidden_layer_sizes = tpl,
         #                                  alpha = 1, activation = 'relu')
         
@@ -83,28 +97,39 @@ class Random_Network:
         self.fitness = net.score(X_test, y_test)            
         
     def Crossover1(self, RandNet): #sommo i contributi tagliati in x e y 
-        son1 = []                  #dalle due network
-        son2 = []
+        son1 = Random_Network()                  #dalle due network
+        son2 = Random_Network()
         x = randint(0,len(self.genome))
-        y = randint(0,len(self,RandNet.genome))
-        son1 = self.genome[:x] + RandNet.genome[y:]
-        son2 = self.genome[x:] + RandNet.genome[:y]
+        y = randint(0,len(RandNet.genome))
+        son1.genome = self.genome[:x] + RandNet.genome[y:]
+        son2.genome = self.genome[x:] + RandNet.genome[:y]
         return son1,son2
             
     def Mutation(self):
-        #mutazione, può anche allungare o accorciare il genoma di 1 carattere
+        #mutazione, può anche allungare o accorciare la rete di 1 layer
         x = randint(0,100)
         y = randint(0,100)
         if x < 10:
             if x < 5:
                 self.genome.append(randint(1,100))
-            elif len(self.genome) != 1:
-                z = randint(0,self.num_hidden_layers - 1)
+            elif len(self.genome) - 1 and len(self.genome):
+                z = randint(0,len(self.genome) - 1)
                 del self.genome[z]
         if y < 10:
-            z = randint(0,len(self.genome) - 1)
-            self.genome[z] = randint(1,100)
-        
+            if len(self.genome) - 1 and len(self.genome):
+                z = randint(0,len(self.genome) - 1)
+                self.genome[z] = randint(1,100)
+            elif len(self.genome) :
+                self.genome[0] = randint(1,100)
+            else :
+                self.genome.append(randint(1,100))
+                
+    def cal_Neurons_product(self):
+        num = 1
+        for ind in self.genome:
+                num = num*ind
+        self.Neurons_product = num 
+                
     def Print_RNet(self):
         print("Hidden_Layer:",self.genome,sep = "\t")
         print("Fitness:",self.fitness,sep = "\t")
